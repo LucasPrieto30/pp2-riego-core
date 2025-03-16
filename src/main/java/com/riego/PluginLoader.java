19f3e4cea3d7df024af3b1ad42673b5d0c4c49d4
Lucas Prieto <lucasprieto30@gmail.com>
2025-03-16 13:06:17 -0300

    Update PluginLoader.java

--- src/main/java/com/riego/PluginLoader.java
+++ src/main/java/com/riego/PluginLoader.java
@@ -3,16 +3,19 @@ package com.riego;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PluginLoader {
-    private static final String PLUGIN_PATH = "plugins/";
+    private static final String PLUGIN_PATH = "plugins/";  // Carpeta donde se almacenan los sensores dinámicos
 
-    public static void cargarPlugins() {
+    public static List<PluginSensor> cargarPlugins() {
+        List<PluginSensor> sensores = new ArrayList<>();
         File pluginDir = new File(PLUGIN_PATH);
+
         if (!pluginDir.exists() || !pluginDir.isDirectory()) {
-            System.out.println("No se encontraron plugins.");
-            return;
+            System.out.println("No se encontró la carpeta de plugins.");
+            return sensores;
         }
 
         for (File file : pluginDir.listFiles()) {
@@ -26,13 +29,15 @@ public class PluginLoader {
                     if (PluginSensor.class.isAssignableFrom(pluginClass)) {
                         PluginSensor sensor = (PluginSensor) pluginClass.getDeclaredConstructor().newInstance();
                         sensor.inicializar();
-                        System.out.println("Valor del sensor: " + sensor.obtenerValor());
+                        sensores.add(sensor);
+                        System.out.println("Sensor dinámico cargado: " + className);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+        return sensores;
     }
 }
 
