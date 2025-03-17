c13e647201ee17456dcb427a9697333a44860166
Lucas Prieto <lucasprieto30@gmail.com>
2025-03-16 23:30:29 -0300

    Update DispositivoRiego.java

--- src/main/java/com/riego/DispositivoRiego.java
+++ src/main/java/com/riego/DispositivoRiego.java
@@ -1,29 +1,42 @@
 package com.riego;
 
+import com.riego.Observer;
+import com.riego.SensorHumedad;
 
 public class DispositivoRiego implements Observer {
     private boolean activo = false;
+    private SensorHumedad sensor;
+
+    public DispositivoRiego(SensorHumedad sensor) {
+        this.sensor = sensor;
+        this.sensor.agregarObservador(this);  // 📌 Se suscribe como observador del sensor
+    }
 
     @Override
-    public void actualizar(Sensor sensor) {
-        if (sensor instanceof SensorHumedad) {
-            SensorHumedad sh = (SensorHumedad) sensor;
-            if (sh.necesitaRiego() && !activo) {
-                activar();
-            } else if (!sh.necesitaRiego() && activo) {
-                desactivar();
-            }
+    public void actualizar(SensorHumedad sensor) {
+        if (sensor.necesitaRiego() && !activo) {
+            activar();
+        } else if (!sensor.necesitaRiego() && activo) {
+            desactivar();
         }
     }
 
     public void activar() {
-        activo = true;
-        System.out.println("💧 Riego ACTIVADO");
+        if (!activo) {
+            activo = true;
+            System.out.println("💧 Riego ACTIVADO");
+        }
     }
 
     public void desactivar() {
-        activo = false;
-        System.out.println("💧 Riego DESACTIVADO");
+        if (activo) {
+            activo = false;
+            System.out.println("💧 Riego DESACTIVADO");
+        }
+    }
+
+    public boolean estaActivo() {
+        return activo;
     }
 }
 
