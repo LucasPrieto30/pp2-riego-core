b1f3503470a9d4e17232691a8e3f5fc2034d2b7d
LucasPrieto30 <lucasprieto30@gmail.com>
2025-04-25 17:20:02 -0300

    tests

--- src/test/java/smartwater/US1.java
+++ src/test/java/smartwater/US1.java
@@ -22,14 +22,14 @@ public class US1 {
     @Test
     public void ca1RiegoActivado() throws InterruptedException {
     	 SmartWater smartWater = SmartWaterFactory.crear(ruta_sensor_mide_1, ruta_umbral);
-         Thread.sleep(3000); // esperar que mida
+         Thread.sleep(4000); // esperar que mida
          assertTrue(smartWater.riegoActivado(), "Debería activarse el riego (medición 1, umbral 2)");
     }
 
     @Test
     public void ca2RiegoNoActivado() throws InterruptedException {
    	 	SmartWater smartWater = SmartWaterFactory.crear(ruta_sensor_mide_3, ruta_umbral);
-        Thread.sleep(3000); // esperar que mida
+        Thread.sleep(4000); // esperar que mida
         assertFalse(smartWater.riegoActivado(), "No debería activarse el riego (medición 3, umbral 2)");
     }
 }
